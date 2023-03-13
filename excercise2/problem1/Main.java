ef86fbcd915d2181f89cb8fa6a0465ff8f9e6596
rubeshudayakumar <rubesh.udayakumar@cdw.com>
2023-03-13 22:53:45 +0530

    commit

---
+++ excercise2/problem1/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // creating the test class object 
+        TestClass object = new TestClass();
+        object.throwException();
+        
+    }
+}
---
+++ excercise2/problem1/TestClass.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+
+public class TestClass {
+
+    public void throwException(){
+    
+        // creating the object of the class IOException
+        IOException ioExceptionObject = new IOException();
+        throw ioExceptionObject;
+
+    }
+
+}
+
+
+
+
